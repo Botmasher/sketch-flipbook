50fc3dd2085f8ea2f8d6b09b25647c71b8555342
Botmasher <languagejosh@hotmail.com>
2018-11-05 10:39:19 -0800

    Integrate flipbook into public index and app component

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This is a digital sketch flip-through notebook for showcasing early sketchwork f
 The app was created with React JS, but the core mechanic is handled in `/public` between the HTML and jQuery. To edit the app locally:
 
 - get a copy of this repo on your local machine
-- install purchased Real3D FlipBook files under `/public/real3d-flipbook/`
+- install purchased Real3D FlipBook `/deploy` files under `/public/real3d-flipbook/`
 - edit `/public/index.html`
   - to reflect desired flipbook settings
   - to contain desired flipbook pages (images or PDF)
--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const thumbSrc = 'real3d-flipbook/images/book2/thumb1.jpg';
+    const sentences = ['Real 3D Flipbook has lightbox feature - book can be displayed in the same page with lightbox effect.', 'Click on a book cover to start reading.'];
     return (
       <div className="App">
         <header className="App-header">Sketches Notebook</header>
+        <div id="container">
+          {sentences.map((sentence, id) => (
+            <p key={`sentence-${id}`}>{sentence}</p>
+          ))}
+          <img src={thumbSrc} />
+        </div>
       </div>
     );
   }
