b482bfbecd420125f8a087bc70b9d9f44ca6761c
Botmasher <languagejosh@hotmail.com>
2018-11-16 05:56:26 -0500

    Update pdf link from inside app using promise

--- src/App.js
+++ src/App.js
@@ -9,39 +9,45 @@ class App extends Component {
     super(props);
     this.state = {
       usePages: false,
-      usePdf: true
+      usePdf: true,
+      displayFlipbook: false
     };
   }
 
   componentDidMount() {
-    console.log("looking for refs...");
-    console.log(this.refs.flipbook);
-    // this.refs.flipbook.flipBook({
-    //   pdfUrl: 'real3d-flipbook/pdf/book2.pdf'
-    // });
+
   }
 
+  // TODO move pdf links into store
   handleClick = e => {
     e.preventDefault();
-    console.log("click pdf load callback run in index.html - now set UI here");
+    const pdfLink = 'real3d-flipbook/pdf/book2.pdf';
+    this.setState({ displayFlipbook: true }, () => {
+      this.refs.sketchbook1.flipbookPdfPromise.then(loadFlipbook => (
+        loadFlipbook(pdfLink)
+      ));
+    });
   };
 
   render() {
-    const { usePages, usePdf } = this.state;
-    console.log("in App component");
+    const { usePages, usePdf, displayFlipbook } = this.state;
     return (
       <div className="app">
         <header className="app-header">Sketches Notebook</header>
-        <div id="flipbook-container" ref="flipbook">
+        <div className="flipbook-selector">
           <p>`Real 3D Flipbook has lightbox feature - book can be displayed in the same page with lightbox effect.`</p>
           <p>`Click on a book cover to start reading.`</p>
-          <a className="flipbook-pdf" onClick={e => this.handleClick(e)} href="real3d-flipbook/pdf/book2.pdf">
+          <a className="flipbook-pdf" onClick={e => this.handleClick(e)} href="my-sketchbook" ref="sketchbook1">
             <img src={thumbs[0]} alt="flipbook thumbnail" />
           </a>
-          <Reader />
-          {pages && usePages && (<div className="pages-test"></div>)}
-          {usePdf && (<div className="pdf-test"></div>)}
         </div>
+        {displayFlipbook && (
+          <div id="flipbook-container">
+            <Reader />
+            {pages && usePages && (<div className="pages-test">using pages</div>)}
+            {usePdf && (<div className="pdf-test">using pdf</div>)}
+          </div>
+        )}
       </div>
     );
   }
--- src/Reader.js
+++ src/Reader.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 export const Reader = () => {
-  console.log("looking at Reader component");
   return(
     <div className="reader">( Reader )</div>
   );
