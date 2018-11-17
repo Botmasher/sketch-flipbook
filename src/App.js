c3f25b8745aa3aad42c2bb1778c3b1cc782defc7
Botmasher <languagejosh@hotmail.com>
2018-11-17 10:18:21 -0500

    Parse pdf loading urls and handle clicks both in app and index

--- src/App.js
+++ src/App.js
@@ -10,34 +10,58 @@ class App extends Component {
     this.state = {
       usePages: false,
       usePdf: true,
-      displayFlipbook: false
+      displayFlipbook: false,
+      flipbooks: {
+        1: {
+          id: 1,
+          title: 'My Sketchbook',
+          thumb: '',
+          url: 'real3d-flipbook/pdf/book2.pdf'
+        },
+        2: {
+          id: 2,
+          title: 'Some Other Book',
+          url: '/sketchbooks/some-other-book.pdf'
+        }
+      }
     };
   }
 
   componentDidMount() {
-
   }
+
+  // TODO better ids and parsing
+  parseSketchbookNumber = url => {
+    console.log(url);
+    const sketchbookIndex = /\d+/.exec(url);
+    const sketchbookNumber = parseInt(sketchbookIndex);
+    console.log(sketchbookNumber);
+    return sketchbookNumber;
+  };
 
   // TODO move pdf links into store
   handleClick = e => {
     e.preventDefault();
+    const { flipbooks } = this.state;
+    const flipbookId = this.parseSketchbookNumber(e.target.parentNode.pathname);
     const pdfLink = 'real3d-flipbook/pdf/book2.pdf';
     this.setState({ displayFlipbook: true }, () => {
-      this.refs.sketchbook1.flipbookPdfPromise.then(loadFlipbook => (
-        loadFlipbook(pdfLink)
+      this.refs.sketchbook1.flipbookSetPdf.then(loadFlipbook => (
+        loadFlipbook(flipbooks[flipbookId].url)
       ));
     });
   };
 
   render() {
     const { usePages, usePdf, displayFlipbook } = this.state;
+    console.log(displayFlipbook);
     return (
       <div className="app">
         <header className="app-header">Sketches Notebook</header>
         <div className="flipbook-selector">
           <p>`Real 3D Flipbook has lightbox feature - book can be displayed in the same page with lightbox effect.`</p>
           <p>`Click on a book cover to start reading.`</p>
-          <a className="flipbook-pdf" onClick={e => this.handleClick(e)} href="my-sketchbook" ref="sketchbook1">
+          <a className="flipbook-link" onClick={e => this.handleClick(e)} href="sketchbook-1" ref="sketchbook1">
             <img src={thumbs[0]} alt="flipbook thumbnail" />
           </a>
         </div>
--- src/store/index.js
+++ src/store/index.js
@@ -3,3 +3,19 @@ import pagesList from './pages';
 
 export const thumbs = thumbsList;
 export const pages = pagesList;
+
+const testData = {
+  flipbooks: {
+    1: {
+      id: 1,
+      title: 'My Sketchbook',
+      thumb: '',
+      url: 'real3d-flipbook/pdf/book2.pdf'
+    },
+    2: {
+      id: 2,
+      title: 'Some Other Book',
+      url: '/sketchbooks/some-other-book.pdf'
+    }
+  }
+};
